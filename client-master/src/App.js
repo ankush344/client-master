6cb60b56e7ca1368f1186fc283cf5fb5a6ce964b
swagata266 <114409017+ankush344@users.noreply.github.com>
2023-02-06 13:25:50 +0530

    file uploade

---
+++ client-master/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './assets/styles/styles.scss'
+import './App.css'
+import reactLogo from './assets/images/react_logo.png'
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>React App</h1>
+      <img src={reactLogo} alt='react logo' />
+    </div>
+  )
+}
+
+export default App
